261a2984d3b5d69a1ab74072c19df9cbf85dd11b
student <student@imac-19.local>
2018-12-18 13:03:05 +0800

    finish

--- caoqinyu_13/ViewController.swift
+++ caoqinyu_13/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    var label:UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor=UIColor.white
+        //定义按钮
+        let btn=UIButton(frame: CGRect(x: 100, y: 50, width: 100, height: 40))
+        btn.setTitle("start", for: .normal)
+        btn.layer.borderWidth=1
+        btn.setTitleColor(UIColor.black, for: .normal)
+        //btn.setTitleColor(UIColor.brown, for: .highlighted)
+        btn.addTarget(self, action: #selector(startcul), for: .touchUpInside)
+        self.view.addSubview(btn)
+        //定义label
+        label=UILabel(frame: CGRect(x: 75, y: 100, width: 200, height: 40))
+        //默认显示值
+        label.text="sum=0"
+        label.textAlignment = .center
+        label.layer.borderWidth=1
+        label.textColor=UIColor.black
+        self.view.addSubview(label)
     }
-
+    @objc func startcul(){
+        DispatchQueue.global().async {
+            var sum=0
+            for num in 1...9999999{
+                sum+=num
+                //ui操作必须在主线程
+                DispatchQueue.main.async {
+                    self.label.text="sum=\(sum)"
+                }
+            }
+        }
+    }
 
 }
 
